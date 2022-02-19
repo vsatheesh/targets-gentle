150f1dbe50f96a593803bbdc23f7764aa73ab311
vsatheesh <viswanathan.satheesh@gmail.com>
2022-02-19 17:38:50 +0800

    removed summ function

--- _targets.R
+++ _targets.R
@@ -1,13 +1,4 @@
 library(targets)
-# Then, run tar_make() to run the pipeline
-# and tar_read(summary) to view the results.
-
-# Define custom functions and other global objects.
-# This is where you write source(\"R/functions.R\")
-# if you keep your functions in external scripts.
-summ <- function(dataset) {
-  summarize(dataset, mean_x = mean(x))
-}
 
 # Set target-specific options such as packages.
 tar_option_set(packages = c("dplyr", "psych"))
